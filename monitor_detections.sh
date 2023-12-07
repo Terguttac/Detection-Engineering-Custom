8253e8aeff30b7b26e49dc197e0344e8a0e9d71e
Sean Dixon <72320127+Terguttac@users.noreply.github.com>
2023-12-07 15:28:15 -0800

    Removed extraneous variable

--- monitor_detections.sh
+++ monitor_detections.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-inotifywait -q -e modify,moved_to -m detections/validated/ detections/failed/ |  while read -r directory events filename path; do python3 ./development/custom_validation.py $filename $(pwd)/$directory$filename$path $events;done
+inotifywait -q -e modify,moved_to -m detections/validated/ detections/failed/ |  while read -r directory events filename; do python3 ./development/custom_validation.py $filename $(pwd)/$directory$filename $events;done
